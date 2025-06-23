9dfebde9159567a70b2c1ef4116f57d899cdcc67
Faven <timafey.makarov@inbox.ru>
2025-06-23 18:37:20 +0600

    test6выфвыфвы

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'backcross-production.up.railway.app',
+  baseURL: 'https://backcross-production.up.railway.app',
 });
 
 instance.interceptors.request.use((config) => {
